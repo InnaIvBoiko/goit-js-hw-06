c668ea8192f95bd6604f16050f5e2f4fea4f6adc
Inna Boiko <146464243+InnaIvBoiko@users.noreply.github.com>
2026-04-06 17:12:38 +0200

    Format code with Prettier and improve variable naming in task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,25 +1,25 @@
 class Storage {
-    #items;
-    constructor(items) {
-        this.#items = items;
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const itemIndex = this.#items.indexOf(itemToRemove);
+    if (itemIndex !== -1) {
+      this.#items.splice(itemIndex, 1);
     }
-    getItems() {
-      return this.#items;
-    } 
-    addItem(newItem){
-        this.#items.push(newItem);
-    }
-    removeItem(itemToRemove) {
-        let index = this.#items.indexOf(itemToRemove);
-        if (index !== -1) {
-            this.#items.splice(index, 1);
-        }
-    }
+  }
 }
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
+storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
+storage.removeItem('Prolonger');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
